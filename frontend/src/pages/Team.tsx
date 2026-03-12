42c3ea26ed0b568ba1396e455476345864b08626
danaxsax <dana.salinas.tareas@gmail.com>
2026-03-12 13:38:28 -0600

    feat: album cards

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -1,13 +1,43 @@
-import CardsList from "./CardList";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import { Experience } from "../components/Experience";
+import { AlbumUI } from "../components/UI";
+import { getCards } from "../services/cardsService";
+import type { Card } from "../services/cardsService";
 
 function Team() {
-  return (
-    <div className="team-page">
-      <h1 className="team-title">
-        Equipo - <span>Álbum de Jugadores</span>
-      </h1>
+  const [cards, setCards] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(true);
 
-      <CardsList />
+  useEffect(() => {
+    getCards().then((data) => {
+      setCards(data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="team-album-loading">
+        <p>Cargando álbum...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="team-album-container">
+      <AlbumUI cards={cards} />
+      <Canvas
+        shadows
+        camera={{
+          position: [-0.5, 1, window.innerWidth > 800 ? 4 : 9],
+          fov: 45,
+        }}
+      >
+        <Suspense fallback={null}>
+          <Experience cards={cards} />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
