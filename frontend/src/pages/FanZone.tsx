2d2852d15c37c0381b230930dd8bac54ebd919d1
Angel Garcia <A00837832@tec.mx>
2026-03-12 14:48:34 -0600

    avatar

--- frontend/src/pages/FanZone.tsx
+++ frontend/src/pages/FanZone.tsx
@@ -1,11 +1,16 @@
 import RankingTable from "../components/RankingTable";
 import "./FanZone.css";
+import Barraselect from "../components/avatar/Barraselect";
 
 function FanZone() {
   return (
     <div>
       <h1 className="FanZone-title">Fan Ranking</h1>
       <RankingTable />
+
+      <div className="barra-select">
+        <Barraselect />
+      </div>
     </div>
   );
 }
