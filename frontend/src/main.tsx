4be499c499a916390d7b5b371c68cfdea976e5e4
Fernando <A01425797@tec.mx>
2026-04-14 00:02:55 -0600

    noticias para usuario

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // <-- IMPORTA ESTO
 import {App} from './App'
 import './index.css'
+import "./styles/pages.css";
+import "./styles/components.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
