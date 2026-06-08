cc41ee579d0b802a595750df56bef4c2bfc9b0d3
Noarsy11-coder <a01638951@tec.mx>
2026-06-08 14:05:35 -0600

    Cambios

--- frontend/src/pages/Juego.tsx
+++ frontend/src/pages/Juego.tsx
@@ -9,7 +9,7 @@ export function Juego() {
   
   const { user, isAuthenticated } = useAuth();
 
-  const unityGameBaseUrl = "https://jovial-crumble-b3ee62.netlify.app";
+  const unityGameBaseUrl = "https://brilliant-medovik-333101.netlify.app";
 
   const unityGameUrl = user?.id
     ? `${unityGameBaseUrl}?userId=${encodeURIComponent(user.id)}`
