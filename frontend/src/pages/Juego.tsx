5df0c73b6ebeacbb62d6469b2575f430bc862e29
Noarsy11-coder <a01638951@tec.mx>
2026-06-08 19:05:03 -0600

    cambio link final

--- frontend/src/pages/Juego.tsx
+++ frontend/src/pages/Juego.tsx
@@ -9,7 +9,7 @@ export function Juego() {
   
   const { user, isAuthenticated } = useAuth();
 
-  const unityGameBaseUrl = "https://fancy-palmier-f42312.netlify.app/";
+  const unityGameBaseUrl = "https://creative-douhua-eb07a6.netlify.appp/";
 
   const unityGameUrl = user?.id
     ? `${unityGameBaseUrl}?userId=${encodeURIComponent(user.id)}`
